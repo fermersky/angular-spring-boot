85f8054a081d577538a81c7b9f222f6ef32f1173
fermersky <forte201253@gmail.com>
2020-05-28 21:08:45 +0300

    footer component

--- ClientApp/src/app/core/core.module.ts
+++ ClientApp/src/app/core/core.module.ts
@@ -1,15 +1,22 @@
+// anuglar
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
-import { HeaderComponent } from './header/header.component';
+// modules
 import { MaterialModule } from '../material/material.module';
+
+// guards
+import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
+
+// components
+import { HeaderComponent } from './header/header.component';
 import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MainContainerComponent],
+  declarations: [HeaderComponent, MainContainerComponent, FooterComponent],
   imports: [MaterialModule, RouterModule],
-  exports: [HeaderComponent, MainContainerComponent],
+  exports: [HeaderComponent, MainContainerComponent, FooterComponent],
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
