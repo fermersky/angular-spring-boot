095b814d70c504d65d39c325bf463085781731f6
femersky <forte201253@gmail.com>
2020-06-06 13:27:48 +0300

    remove useless bytes compression and decompression of files

--- src/main/java/org/fermer/danielapi/models/File.java
+++ src/main/java/org/fermer/danielapi/models/File.java
@@ -68,47 +68,11 @@ public class File {
     //
 
     public byte[] getPicBytes() {
-        return decompressBytes(picBytes);
+        return this.picBytes;
     }
 
     public void setPicBytes(byte[] picBytes) {
-        this.picBytes = compressBytes(picBytes);
-    }
-
-    public byte[] compressBytes(byte[] data) {
-        System.out.println("compressed");
-
-        Deflater deflater = new Deflater();
-        deflater.setInput(data);
-        deflater.finish();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] buffer = new byte[1024];
-        while (!deflater.finished()) {
-            int count = deflater.deflate(buffer);
-            outputStream.write(buffer, 0, count);
-        }
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-        }
-        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
-        return outputStream.toByteArray();
-    }
-    public byte[] decompressBytes(byte[] data) {
-        Inflater inflater = new Inflater();
-        inflater.setInput(data);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] buffer = new byte[1024];
-        try {
-            while (!inflater.finished()) {
-                int count = inflater.inflate(buffer);
-                outputStream.write(buffer, 0, count);
-            }
-            outputStream.close();
-        } catch (IOException ioe) {
-        } catch (DataFormatException e) {
-        }
-        return outputStream.toByteArray();
+        this.picBytes = picBytes;
     }
 
 }
