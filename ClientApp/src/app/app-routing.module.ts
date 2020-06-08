12f104aee3e82184145c1ccb041fc4af75ec8a9b
femersky <forte201253@gmail.com>
2020-06-09 00:43:57 +0300

    do not scroll page to the previous position after changing url

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -27,6 +27,6 @@ const routes: Routes = [
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
 })
 export class AppRoutingModule {}
