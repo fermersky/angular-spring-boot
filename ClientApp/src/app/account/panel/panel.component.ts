c1c92392ba6d38fa183f5a462e80e357089cce6b
femersky <forte201253@gmail.com>
2020-06-09 00:36:27 +0300

    entity tables in the admin panel

--- ClientApp/src/app/account/panel/panel.component.ts
+++ ClientApp/src/app/account/panel/panel.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { DatabaseInfoService } from 'src/app/core/services/database.info.service';
-import { IEntityInfoDto } from 'src/app/core/interfaces';
+import { IEntityInfoDto, IFetchedBook, IFetchedAuthor } from 'src/app/core/interfaces';
+import { BooksService } from 'src/app/core/services/books.service';
+import { Observable } from 'rxjs';
+import { SubSink } from 'subsink';
+import { AuthorsService } from 'src/app/core/services/authors.service';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css'],
 })
-export class PanelComponent implements OnInit {
+export class PanelComponent implements OnInit, OnDestroy {
   entitiesInfo: IEntityInfoDto[];
+  books$: Observable<IFetchedBook[]>;
+  authors$: Observable<IFetchedAuthor[]>;
 
-  constructor(private dbInfoService: DatabaseInfoService) {}
+  subsink = new SubSink();
+
+  constructor(
+    private dbInfoService: DatabaseInfoService,
+    private booksService: BooksService,
+    private authorsService: AuthorsService
+  ) {}
 
   ngOnInit(): void {
-    this.dbInfoService.get().subscribe((data) => {
+    this.subsink.sink = this.dbInfoService.get().subscribe((data) => {
       this.entitiesInfo = data;
     });
+
+    this.books$ = this.booksService.get();
+    this.authors$ = this.authorsService.get(false);
+  }
+
+  ngOnDestroy(): void {
+    this.subsink.unsubscribe();
   }
 }
